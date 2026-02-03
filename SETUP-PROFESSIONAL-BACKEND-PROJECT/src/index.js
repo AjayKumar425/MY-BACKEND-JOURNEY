6cc1e2ce5eff274a160a7f86029be437fa131b4c
Ajay <ajjuu425@gmail.com>
2026-02-03 23:16:36 +0530

    connectiviy success

---
+++ SETUP-PROFESSIONAL-BACKEND-PROJECT/src/index.js
@@ -0,0 +1,67 @@
+
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+
+import mongoose, { connect } from "mongoose" ;
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+
+
+dotenv.config({
+    path : './env'
+})
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+
+( async () => {
+
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", () => {
+            console.log("error" , error);
+            throw error
+        })
+
+         app.listen(process.env.PORT , () =>{
+            console.log(`app is listening on port ${
+                
+                process.env.PORt
+             }`)
+         })
+
+
+    } catch (error) {
+        console.error("ERROR:" , error)
+        throw err
+    }
+})()
+
+
+    */

d637e0437f4b981770b7379ca5b79347de5c3823
Ajay <ajjuu425@gmail.com>
2026-02-04 15:56:28 +0530

    updated agin 4feb

--- SETUP-PROFESSIONAL-BACKEND-PROJECT/src/models/user.model.js
+++ SETUP-PROFESSIONAL-BACKEND-PROJECT/src/models/user.model.js
@@ -10,9 +10,53 @@ const userSchema = new Schema(
             lowercase: true,
             trim : true , 
             index : true,
+        },
+        email : {
+             type : String ,
+            required : true,
+            unique : true , 
+            lowercase: true,
+            trim : true , 
+            
+        },
+        fullname : {
+             type : String ,
+            required : true,
+            
+            lowercase: true,
+            trim : true , 
+            index : true
+        },
+        avatar  :{
+            type : String , // cloudinary url
+            required : true ,
+
+        }, 
+        coverImage : {
+            type : String , // cloudinary url
+            
+        },
+        watchHistory : [
+            {
+                type: Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ], 
+        password : {
+            type : String,
+            required : [true , 'password is required']
+        },
+        refreshToken : {
+            type : String,
+
         }
 
         
+
+
+    },
+    {
+        timestamps: true
     }
 
 
